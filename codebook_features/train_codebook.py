5c84dc1bc6ef3c99bfa5d07ff2b3f03b3b90150c
Ilya Lasy <ilyalas@gmail.com>
2024-06-13 14:13:57 +0200

    support training from scratch

--- codebook_features/train_codebook.py
+++ codebook_features/train_codebook.py
@@ -1,19 +1,45 @@
 """Train script for Codebook models."""
 
-import dataclasses
 import json
+import logging
 import os
+import pathlib
 import sys
+from dataclasses import dataclass, field, replace
+from typing import Optional
 
 import hydra
 import omegaconf
 import pandas as pd
 import torch
 import transformers
-import wandb
+from transformers import GPT2Config, GPT2LMHeadModel, GPTNeoXConfig, GPTNeoXForCausalLM
 
+import wandb
 from codebook_features import models, run_clm
 from codebook_features import trainer as cb_trainer
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class ModelConfigArguments:
+    """Arguments pertaining to which model/config/tokenizer we are going to fine-tune, or train from scratch."""
+
+    model_path: Optional[str] = field(
+        default=None,
+        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"},
+    )
+    continue_training: bool = field(default=False)
+    model_type: str = field(default="gptneox")
+    hidden_size: int = field(default=128)
+    intermediate_size: int = field(default=512)
+    num_hidden_layers: int = field(default=4)
+    num_attention_heads: int = field(default=4)
+    rotary_emb_base: int = field(default=10000)
+    seq_len: int = field(default=128)
+    vocab_size: int = field(default=11)
+
 
 # shortened arg names to compress wandb titles
 shortened_args = {
@@ -53,7 +79,7 @@ def prepare_logging(cfg):
 def get_baseline(training_args, model_args, data_args, model):
     """Get baseline metrics for the original model (no codebooks applied)."""
     baseline_output_dir = training_args.output_dir + "_baseline"
-    eval_args = dataclasses.replace(
+    eval_args = replace(
         training_args,
         output_dir=baseline_output_dir,
     )
@@ -105,7 +131,45 @@ def get_optimizer(training_args, model):
     return optimizer
 
 
-@hydra.main(config_path="config", config_name="main", version_base=None)
+def load_model(config_args: ModelConfigArguments):
+    """Load the model based on the config."""
+    if config_args.model_path is not None and config_args.continue_training:
+        model = transformers.AutoModelForCausalLM.from_pretrained(config_args.model_path)
+    elif config_args.model_path is not None:
+        config = transformers.AutoConfig.from_pretrained(config_args.model_path)
+        model = transformers.AutoModelForCausalLM.from_config(config)
+        logger.info("Initialized model from config.")
+    elif config_args.model_type == "gptneox":
+        config = GPTNeoXConfig(
+            vocab_size=config_args.vocab_size,
+            hidden_size=config_args.hidden_size,
+            num_hidden_layers=config_args.num_hidden_layers,
+            num_attention_heads=config_args.num_attention_heads,
+            intermediate_size=config_args.intermediate_size,
+            rotary_emb_base=config_args.rotary_emb_base,
+            bos_token_id=config_args.vocab_size - 1,
+            eos_token_id=config_args.vocab_size - 1,
+            max_position_embeddings=config_args.seq_len,
+        )
+        model = GPTNeoXForCausalLM(config=config)
+    elif config_args.model_type == "gpt2":
+        config = GPT2Config(
+            vocab_size=config_args.vocab_size,
+            n_embd=config_args.hidden_size,
+            n_layer=config_args.num_hidden_layers,
+            n_head=config_args.num_attention_heads,
+            n_inner=config_args.intermediate_size,
+            bos_token_id=config_args.vocab_size - 1,
+            eos_token_id=config_args.vocab_size - 1,
+            max_position_embeddings=config_args.seq_len,
+        )
+        model = GPT2LMHeadModel(config=config)
+    else:
+        raise ValueError(f"Unknown model type {config_args.model_type}")
+    return model
+
+
+@hydra.main(config_path="config", config_name="main_tinystories", version_base=None)
 def main(cfg):
     """Train codebook based models parametrized using hydra.
 
@@ -120,8 +184,10 @@ def main(cfg):
         **(cfg_dict["training_args"]),
         local_rank=local_rank,
     )
-    model_args = run_clm.ModelArguments(**cfg.model_args)
+
+    model_args = run_clm.ModelArguments(**cfg.get("model_args", {}))
     data_args = run_clm.DataTrainingArguments(**cfg.data_args)
+    config_args = ModelConfigArguments(**cfg.model_config_args)
 
     wandb_initilized = False
     if training_args.local_rank <= 0 and "wandb" in training_args.report_to:
@@ -137,10 +203,7 @@ def main(cfg):
         training_args.output_dir = pathlib.Path(wandb.run.dir).parent / "train_output"
         training_args.output_dir = str(training_args.output_dir)
 
-    model = transformers.AutoModelForCausalLM.from_pretrained(
-        model_args.model_name_or_path,
-        cache_dir=model_args.cache_dir,
-    )
+    model = load_model(config_args)
 
     if cfg.get_baseline:
         return get_baseline(training_args, model_args, data_args, model)
